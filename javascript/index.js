ad3d3ae6d0058b4fca1625a79f486e557cce67e0
nessasaddi <134745638+nessasaddi@users.noreply.github.com>
2023-11-05 20:35:22 -0800

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -24,3 +24,23 @@ function updateTime() {
     );
   }
 }
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+      <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small></div>
+      </div>
+      `;
+}
