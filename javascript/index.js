b0a6e593961e33ee72cbb7f81fd783806ea785d9
Vanessa Saddi <134745638+nessasaddi@users.noreply.github.com>
2024-01-05 00:49:48 -0800

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,4 @@
 function updateTime() {
-  // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
     let losAngelesDateElement = losAngelesElement.querySelector(".date");
@@ -12,7 +11,6 @@ function updateTime() {
     );
   }
 
-  // Paris
   let parisElement = document.querySelector("#paris");
   if (parisElement) {
     let parisDateElement = parisElement.querySelector(".date");
@@ -25,7 +23,6 @@ function updateTime() {
     );
   }
 
-  // Cairo
   let cairoElement = document.querySelector("#cairo");
   if (cairoElement) {
     let cairoDateElement = cairoElement.querySelector(".date");
